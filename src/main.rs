8d119502ce26b256fecc1e388e4c3ff56bf2b4a3
bert-bae <elbert.bae@gmail.com>
2023-09-27 15:56:28 -0700

    Add progress spinner

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use clap::Parser;
+use indicatif::{ProgressBar, ProgressStyle};
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::time::Duration;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser, Debug)]
@@ -13,10 +15,10 @@ struct Cli {
     // #[arg(short = 'f', long = "filepath")]
     path: std::path::PathBuf,
     #[arg(short = 'c', long = "case_sensitive", default_value_t = false)]
-    case_sensitive: bool
+    case_sensitive: bool,
 }
 
-fn find_lines(mut args: Cli) -> Result<Vec<String>, std::io::Error> { 
+fn find_lines(mut args: Cli, pb: &ProgressBar) -> Result<Vec<String>, std::io::Error> {
     if args.case_sensitive == true {
         args.pattern = args.pattern.to_lowercase();
     }
@@ -28,6 +30,7 @@ fn find_lines(mut args: Cli) -> Result<Vec<String>, std::io::Error> {
     let mut line = String::new();
     let mut current_line = 1;
     while reader.read_line(&mut line)? != 0 {
+        pb.tick();
         let matching_line: bool;
         if args.case_sensitive {
             matching_line = line.to_lowercase().contains(&args.pattern);
@@ -41,13 +44,20 @@ fn find_lines(mut args: Cli) -> Result<Vec<String>, std::io::Error> {
         current_line += 1;
         line.clear();
     }
-    
-    return Ok(found)
+
+    return Ok(found);
 }
 
 fn main() {
     let args = Cli::parse();
-
-    let found = find_lines(args);
+    let progress_bar = ProgressBar::new_spinner();
+    progress_bar.enable_steady_tick(Duration::from_millis(200));
+    progress_bar.set_style(
+        ProgressStyle::with_template("{spinner:.dim.bold} cargo: {wide_msg}")
+            .unwrap()
+            .tick_chars("/|\\- "),
+    );
+    let found = find_lines(args, &progress_bar);
+    progress_bar.finish_and_clear();
     println!("{found:#?}")
 }
